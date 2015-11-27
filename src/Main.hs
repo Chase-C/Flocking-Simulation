7e0c04b112f7ecdfc017392957ce3ad3512e04ec
Chase Cummings <chaseecummings@gmail.com>
2015-11-26 23:15:08 -0800

    Refactored some code into separate files

--- src/Main.hs
+++ src/Main.hs
@@ -4,14 +4,12 @@ module Main (main) where
 
 --------------------------------------------------------------------------------
 
-import Prelude                 hiding ((<*))
 import System.IO
 import Control.Parallel.Strategies    (parMap, rdeepseq)
 import Control.Monad                  (unless, when, void)
 import Control.Monad.Trans.RWS.Strict (RWST, ask, asks, evalRWST, get, gets, modify, put)
 import Control.Monad.IO.Class         (liftIO)
 import Control.Monad.Trans.Class      (lift)
-import Data.Boolean                   (ifB, (>*))
 
 import Graphics.GPipe
 import qualified Graphics.GPipe.Context.GLFW as GLFW
@@ -20,6 +18,7 @@ import Linear
 
 import Boid
 import EventManager
+import Shader
 import Utils
 import Log
 
@@ -35,8 +34,7 @@ data Env os c ds = Env
     , envMVP           :: Buffer os (Uniform (V4 (B4 Float)))
     , envBoidPositions :: Buffer os (B3 Float, B3 Float)
     , envBoidVerts     :: Buffer os (B3 Float, B3 Float)
-    , envShader        :: PrimitiveArray Triangles ((B3 Float, B3 Float), (B3 Float, B3 Float))
-                          -> Render os (ContextFormat c ds) ()
+    , envShader        :: BoidShader os c ds
     }
 
 data State = State
@@ -136,50 +134,6 @@ main = do
 
         liftIO $ swapInterval 1
         runSim env state
-
-transformStream :: (IfB a, OrdB a, Floating a) => M44 a -> ((V3 a, V3 a), (V3 a, V3 a)) -> (V4 a, V3 a)
-transformStream mvp ((vert, norm), (pos, dir)) = (pos', color)
-    where
-        normDir      = vNorm dir
-        axis         = vNorm $ cross (V3 0 0 1) normDir
-        aAxis        = vNorm $ cross normDir axis
-        rotationMat  = transpose $ V3 axis aAxis normDir
-        transformMat = mvp !*! mkTransformationMat rotationMat pos
-        pos'         = transformMat !* makeV4 vert
-        rNorm        = rotationMat  !* norm
-        (V4 _ _ d _) = mvp          !* makeV4 pos
-        color        = makeColor rNorm d
-
-{-# INLINE makeV4 #-}
-makeV4 :: Num a => V3 a -> V4 a
-makeV4 (V3 x y z) = V4 x y z 1
-
-{-# INLINE interpolate #-}
-interpolate :: (IfB a, OrdB a, Floating a) => V3 a -> V3 a -> a -> V3 a
-interpolate v1 v2 t = ((1 - t') *^ v1) ^+^ (t' *^ v2)
-    where t' = sClamp 0 1 t
-
-{-# INLINE sClamp #-}
-sClamp :: (IfB a, OrdB a, Floating a) => a -> a -> a -> a
-sClamp lower upper val = minB upper (maxB lower val)
-
-{-# INLINE makeColor #-}
-makeColor :: (IfB a, OrdB a, Floating a) => V3 a -> a -> V3 a
-makeColor normal depth = color'
-    where lightVal = normal `dot` V3 0 1 0
-          cLight x = (0.6 + (x / 1.75)) *^ V3 0.2 0.4 0.8
-          cDark  x = (0.9 + (x / 1.75)) *^ V3 0.2 0.1 0.5
-          fadeVal  = (100 - depth) / 80
-          color    = caseB lightVal
-                         [ ((>*   0.1),  cLight lightVal)
-                         , ((<* (-0.1)), cDark  lightVal)
-                         ]
-                         (interpolate (cDark (-0.1)) (cLight 0.1) ((0.1 + lightVal) * 5))
-          color'   = interpolate (V3 0.0 0.015 0.03) color fadeVal
-
-getColorDepth :: V3 FFloat -> RasterizedInfo -> (V3 FFloat, FFloat)
-getColorDepth color info = (color, depth)
-    where (V4 _ _ depth _) = rasterizedFragCoord info
 
 --------------------------------------------------------------------------------
 
